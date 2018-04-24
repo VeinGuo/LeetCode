069ce2d10fc92b81b3a2cfb139a609e258ef90d7
VeinGuo <gwrcode@gmail.com>
2018-04-24 21:40:25 +0800

    [add] 20 Valid Parentheses

---
+++ 20/20-Valid_Parentheses.playground/Contents.swift
@@ -0,0 +1,94 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+ 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ 
+ 有效字符串需满足：
+ 
+ 左括号必须用相同类型的右括号闭合。
+ 左括号必须以正确的顺序闭合。
+ 注意空字符串可被认为是有效字符串。
+ 
+ 示例 1:
+ 
+ 输入: "()"
+ 输出: true
+ 示例 2:
+ 
+ 输入: "()[]{}"
+ 输出: true
+ 示例 3:
+ 
+ 输入: "(]"
+ 输出: false
+ 示例 4:
+ 
+ 输入: "([)]"
+ 输出: false
+ 示例 5:
+ 
+ 输入: "{[]}"
+ 输出: true
+ */
+
+/*
+ Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ 
+ An input string is valid if:
+ 
+ Open brackets must be closed by the same type of brackets.
+ Open brackets must be closed in the correct order.
+ Note that an empty string is also considered valid.
+ 
+ Example 1:
+ 
+ Input: "()"
+ Output: true
+ Example 2:
+ 
+ Input: "()[]{}"
+ Output: true
+ Example 3:
+ 
+ Input: "(]"
+ Output: false
+ Example 4:
+ 
+ Input: "([)]"
+ Output: false
+ Example 5:
+ 
+ Input: "{[]}"
+ Output: true
+ */
+
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        guard s.count > 0 else {
+            return false
+        }
+        var stack = [Character]()
+        for char in s {
+            if char == "(" || char == "{" || char == "[" {
+                stack.append(char)
+            } else if char == ")" {
+                guard stack.count > 0, stack.removeLast() == "(" else {
+                    return false
+                }
+            } else if char == "}" {
+                guard stack.count > 0, stack.removeLast() == "{" else {
+                    return false
+                }
+            } else if char == "]" {
+                guard stack.count > 0, stack.removeLast() == "[" else {
+                    return false
+                }
+            }
+        }
+        return stack.isEmpty
+    }
+}
+
+print(Solution().isValid("{[]}"))
